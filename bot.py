4fceecbbbd04d5be87e7757de6d2f651dfa2903d
Ambro17 <ambro17.1@gmail.com>
2023-09-07 15:40:58 -0300

    ✨ Add ranking

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
 import os
+import textwrap
 
 from telegram import Update
 from telegram.constants import ChatAction
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from api import get_current_matches_as_string
+import requests
 
 from functools import wraps
 
@@ -33,10 +35,49 @@ async def get_matches(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
     )
 
 
+@send_action(ChatAction.TYPING)
+async def get_matches(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_markdown_v2(
+        f'```\n{get_current_matches_as_string()}\n```'
+    )
+
+
+@send_action(ChatAction.TYPING)
+async def ranking(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_markdown_v2(
+        textwrap.dedent(f"""\
+        ```
+        Jugador                 Pais Puntos \#
+        Alejandro Galan         ESP  11950  1
+        Juan Lebron             ESP  11950  1
+        Arturo Coello           ESP  11760  3
+        Martin Di Nenno         ARG  10665  4
+        Franco Stupaczuk        ARG  9360   5
+        Fernando Belasteguin    ARG  9230   6
+        Francisco Navarro       ESP  9070   7
+        Agustin Tapia           ARG  8030   8
+        Federico Chingotto      ARG  7710   9
+        Juan Tello              ARG  5670   10
+        Pablo Lima              BRA  5495   11
+        Carlos Daniel Gutierrez ARG  5140   12
+        Alejandro Ruiz          ESP  4895   13
+        Jeronimo Gonzalez       ESP  4505   14
+        Luciano Capra           ARG  3308   15
+        Maximiliano Sanchez     ARG  3308   15
+        Victor Ruiz             ESP  3035   17
+        Javier Garrido          ESP  2855   18
+        Jorge Nieto             ESP  2790   19
+        Lucas Bergamini         BRA  2755   20
+        ```
+        """)
+    )
+
+
 app = ApplicationBuilder().token(os.environ['BOT_TOKEN']).build()
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("live", get_matches))
+app.add_handler(CommandHandler("ranking", ranking))
 
 print("The bot is running")
 app.run_polling()
